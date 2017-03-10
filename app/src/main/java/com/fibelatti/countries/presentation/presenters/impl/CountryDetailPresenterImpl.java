a379b56bb35cfe36e014694f9d6a696fedab2e03
Filipe Belatti <fibelatti@gmail.com>
2017-03-10 10:21:22 +0100

    Implementing detail presenter.

---
+++ app/src/main/java/com/fibelatti/countries/presentation/presenters/impl/CountryDetailPresenterImpl.java
@@ -0,0 +1,49 @@
+package com.fibelatti.countries.presentation.presenters.impl;
+
+import com.fibelatti.countries.data.remote.impl.CountryRepositoryRemoteImpl;
+import com.fibelatti.countries.data.repository.CountryRepository;
+import com.fibelatti.countries.data.repository.impl.CountryRepositoryImpl;
+import com.fibelatti.countries.presentation.presenters.CountryDetailPresenter;
+import com.fibelatti.countries.presentation.presenters.CountryDetailPresenterView;
+
+import rx.Subscription;
+import rx.android.schedulers.AndroidSchedulers;
+import rx.schedulers.Schedulers;
+
+public class CountryDetailPresenterImpl
+        implements CountryDetailPresenter {
+
+    private CountryDetailPresenterView view;
+    private CountryRepository countryRepository;
+    private Subscription countrySubscription;
+
+    private CountryDetailPresenterImpl(CountryDetailPresenterView view) {
+        this.view = view;
+    }
+
+    public static CountryDetailPresenterImpl createPresenter(CountryDetailPresenterView view) {
+        return new CountryDetailPresenterImpl(view);
+    }
+
+    @Override
+    public void setUp() {
+        countryRepository = new CountryRepositoryImpl(new CountryRepositoryRemoteImpl());
+    }
+
+    @Override
+    public void tearDown() {
+        if (countrySubscription != null && !countrySubscription.isUnsubscribed())
+            countrySubscription.unsubscribe();
+    }
+
+    @Override
+    public void getCountryByName(String countryName) {
+        if (countrySubscription != null && !countrySubscription.isUnsubscribed())
+            countrySubscription.unsubscribe();
+
+        countrySubscription = countryRepository.getByName(countryName)
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe((countries) -> view.displayData(countries.get(0)), (throwable) -> view.handleError(throwable));
+    }
+}
